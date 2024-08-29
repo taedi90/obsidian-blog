b1e89b3e1d4f9869d4cb2dfd40561a9bd3d23f99
taesoo <taesoo@aift.kr>
2024-08-29 11:04:57 +0900

    ㄱㄱ

--- note.md
+++ note.md
@@ -1,2 +1,11 @@
 git add upstream https://github.com/jackyzha0/quartz.git
 git pull upstream v4
+
+git submodule add git@github.com:taedi90/post-archive.git content 
+
+brew install node@22
+
+
+npm i
+
+npx quartz build --serve
--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ko-KR",
     baseUrl: "kimfra.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
