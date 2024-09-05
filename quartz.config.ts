7cff6d49982e3f235dda4e64736babca98a83504
taesoo <taesoo@aift.kr>
2024-09-05 14:56:32 +0900

    Quartz sync: Sep 5, 2024, 2:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,9 +22,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Noto Sans Korean",
+        body: "Nanum Gothic",
+        code: "Nanum Gothic Coding",
       },
       colors: {
         lightMode: {
